0912596a28193fb3b591b7addf5f7cb25ab409a2
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-12-17 21:52:05 -0300

    responsive

---
+++ Navbar e sidebar/script.js
@@ -0,0 +1,9 @@
+var itens = document.getElementById('itens')
+
+function clickMenu(){
+    if(itens.style.display == 'none'){
+        itens.style.display = 'block'
+    }else{
+        itens.style.display = 'none'
+    }
+}
