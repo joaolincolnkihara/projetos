113f93af1794b24e6d834e0cd05420d0b06b684f
João Lincoln <157829347+joaolincolnkihara@users.noreply.github.com>
2024-12-22 21:49:57 -0300

    sidebar

--- Navbar e sidebar/script.js
+++ Navbar e sidebar/script.js
@@ -1,9 +1,15 @@
 var itens = document.getElementById('itens')
 
+// MOBILE
 function clickMenu(){
     if(itens.style.display == 'none'){
         itens.style.display = 'block'
     }else{
         itens.style.display = 'none'
     }
+}
+
+// PC
+function closeBtn(){
+        itens.style.display = 'none'
 }
